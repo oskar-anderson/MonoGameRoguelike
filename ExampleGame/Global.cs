2edd4b5edf4095091db5981d2a62e5a7768090b3
Faron Bracy <signup@dreamersdesign.com>
2014-06-07 18:14:31 -0400

    Creating singleton class for game state

---
+++ ExampleGame/Global.cs
@@ -0,0 +1,14 @@
+﻿namespace ExampleGame
+{
+   public enum GameStates
+   {
+      None = 0,
+      PlayerTurn = 1,
+      EnemyTurn = 2,
+      Debugging = 3
+   }
+   public class Global
+   {
+      public static GameStates GameState { get; set; }
+   }
+}
