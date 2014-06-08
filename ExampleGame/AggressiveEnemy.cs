1804641f6abdd4951afb5088c70204083010a8a3
Faron Bracy <signup@dreamersdesign.com>
2014-06-07 21:12:28 -0400

    New placeholder class for aggressive enemy

---
+++ ExampleGame/AggressiveEnemy.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ExampleGame
+{
+   public class AggressiveEnemy
+   {
+      public int X { get; set; }
+      public int Y { get; set; }
+      public float Scale { get; set; }
+      public Texture2D Sprite { get; set; }
+      public void Draw( SpriteBatch spriteBatch )
+      {
+         float multiplier = Scale * Sprite.Width;
+         spriteBatch.Draw( Sprite, new Vector2( X * multiplier, Y * multiplier ), null, null, null, 0.0f, new Vector2( Scale, Scale ), Color.White, SpriteEffects.None, 0.5f );
+      }
+   }
+}
